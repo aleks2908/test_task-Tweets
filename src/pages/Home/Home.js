86b6396369131447699904c78ffb3208b0998284
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-04-25 16:13:02 +0300

    CardList margins/psddings

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
-import css from "./Home.module.css";
-import { Link } from "react-router-dom";
-import { Header } from "../../components/Header/Header";
+import { Link } from 'react-router-dom';
+import { Header } from '../../components/Header/Header';
+import css from './Home.module.css';
 
 export default function Home() {
   return (
